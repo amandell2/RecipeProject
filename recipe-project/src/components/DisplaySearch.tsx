dd3325036daf4bcd28df4704c6fd3ae7d79baa0c
Shiretha Bellamy <sade.bell937@gmail.com>
2022-04-20 21:24:25 -0400

    Searchfrom

--- recipe-project/src/components/DisplaySearch.tsx
+++ recipe-project/src/components/DisplaySearch.tsx
@@ -1,3 +1,8 @@
+import { useState } from "react";
+import BasicRecipe from "../components/BasicRecipe";
+
+
+
 export default function DisplaySearch(){
     return(
         <div>
--- recipe-project/src/components/SearchForm.tsx
+++ recipe-project/src/components/SearchForm.tsx
@@ -1,7 +1,56 @@
-export default function SearchForm(){
+import {FormEvent, useState} from 'react';
+import {Recipe} from "../models/recipe-model"
+
+// Add call back function from parent
+interface Props {
+    onSubmit:(recipe:Recipe) => void;
+}    
+    
+
+
+function SearchForm() {
+    const [label, setlabel] =useState('');
+    const [dietLabels, setdietLabels] = useState('');
+    const [calories, setCalories] = useState('');
+
+
+
+    
+ function handleSubmit(e:FormEvent) {
+    e.preventDefault();
+    setlabel('');
+    setdietLabels('');
+    setCalories('');
+    
+
+
+}
+
     return(
-        <div>
+        <div className='SearchForm'>
+            <form onSubmit={handleSubmit}>
+                <h2>Search</h2>
+                <p>
+                    <label htmlFor='label'>Keyword</label>
+                    <input id = "label" value = {label} onChange ={e => setlabel(e.target.value)}/> 
+                </p>
+                <p>
+                    <label htmlFor="diet_labels">Restrictions</label>
+                    <input id='diet_labels' value={dietLabels} onChange={e => setdietLabels(e.target.value)}/>
+                </p>
+                <p>
+                    <label htmlFor='calories'>Calories</label>
+                    <input id ="calories" value={calories} onChange={e => setCalories(e.target.value)}/>
+                </p>
+                <input type="submit"value ="Submit"/>
+
+                     
+
+            </form>
+
             
         </div>
     )
 }
+
+export default SearchForm;
